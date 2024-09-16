b30380f04766cd2b33b761cb1038852698fd01c3
maaz <maazkamran65@gmail.com>
2024-09-16 10:38:28 +0500

    final

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const app = express();
 
 connectDB();
 
-// Init Middleware
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/todos', todoRoutes);
+app.use('/auth', authRoutes);
+app.use('/todos', todoRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
