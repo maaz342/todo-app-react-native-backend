ed6d2750d00f425e8d2706c0b58ddf5566d1b4ad
maaz <maazkamran65@gmail.com>
2024-09-20 12:06:08 +0500

    final

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,15 @@
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+
 const auth = (req, res, next) => {
-  const token = req.header('x-auth-token');
+  const token = req.header('Authorization')?.split(' ')[1];
 
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   try {
-    const decoded = jwt.verify(token, 'secretToken'); // Assuming 'secretToken' is your JWT secret
-    req.user = decoded.user; // Attach the user data to the request object
+    const decoded = jwt.verify(token, 'secretToken'); 
+    req.user = decoded.user; 
     next();
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
